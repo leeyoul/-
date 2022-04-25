9b8b59ae531357758f5486cade2849a1af2b0e50
leeyoul <101701164+leeyoul@users.noreply.github.com>
2022-04-25 12:06:18 +0900

    ㅁ

--- geolocation.js
+++ geolocation.js
@@ -1,15 +1,14 @@
 const geo = document.querySelector("#geo");
-const API_Key ="15057754ab06a648d8e2f8ff4eeadf1c"
+const API_Key ="15057754ab06a648d8e2f8ff4eeadf1c";
 
 function onGeoOK(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_Key}`;
-    fetch(url)
-        .then((respone) => response.json())
-        .then((data)=> {
-            geo.innerText = `${data.geo[0].main};`
-        })
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_Key}&units=metric`;
+    fetch(url).then(response => response.json()).then(data=>{
+        const weathername = data.name
+        geo.innerText =  data.weather[0].main;
+    });
     
 }
 
